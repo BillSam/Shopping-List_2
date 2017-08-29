0b016ac93a1561e7eafad4535f692ccecb80349f
Bill Sam <sam.wangui13@gmail.com>
2017-08-29 07:40:57 +0300

    Implemented the web template in flask

--- __init__.py
+++ __init__.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return "Hi there, how you doing?"
+	return render_template("index.html")
+
+@app.route('/about/')
+def about():
+	return render_template("about.html")
+
+
+@app.route('/services/')
+def services():
+	return render_template("services.html")
+
+@app.route('/login/')
+def login():
+	return render_template("login.html")
+
+@app.route('/signup/')
+def signup():
+	return render_template("signup.html")
+
+@app.route('/list/')
+def list():
+	return render_template("list.html")
 
 
 if __name__ == "__main__":
